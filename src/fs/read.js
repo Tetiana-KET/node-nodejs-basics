731a58470c5aa828cecbff17eaf63f76d8b8130e
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-02 20:05:03 +0300

    feat: print content of the fileToRead.txt into console

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const read = async () => {
-    // Write your code here 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+	const readableStream = fs.createReadStream(
+		path.join(__dirname, 'files', 'fileToRead.txt'),
+		'utf8'
+	);
+
+	let data = '';
+	readableStream.on('data', chunk => (data += chunk));
+	readableStream.on('end', () => console.log(data));
+	readableStream.on('error', () =>
+		console.log(
+			'Error: FS operation failed! \nPlease check if such file exist!'
+		)
+	);
 };
 
 await read();
