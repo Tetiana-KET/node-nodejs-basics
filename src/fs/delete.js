ddb56f0bbdfd30d3a9998b8d679f94155b04a442
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 12:07:49 +0300

    feat: implement function that deletes file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+/*implement function that deletes file fileToRemove.txt 
+(if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)*/
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const remove = async () => {
-    // Write your code here 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+	const fileToDelete = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+	try {
+		await fs.promises.access(fileToDelete);
+		fs.promises.unlink(fileToDelete);
+		console.log(`fileToRemove.txt was successfully removed! 👏`);
+	} catch (err) {
+		throw new Error(
+			`FS operation failed.  🕵️   File does not exist! Error: ${err.message}`
+		);
+	}
 };
 
 await remove();
