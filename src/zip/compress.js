96d41212ca661d1283394c919b122367bdde59be
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 21:19:30 +0300

    feat: implement function that decompresses archive.gz back to the fileToCompress.txt

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -14,25 +14,30 @@ const compress = async () => {
 	const fileToCompress = path.join(__dirname, 'files', 'fileToCompress.txt');
 	const compressedFile = path.join(__dirname, 'files', 'archive.gz');
 
-	const readableStream = fs.createReadStream(fileToCompress);
-	const writableStream = fs.createWriteStream(compressedFile);
-
-	const gzip = zlib.createGzip();
-
 	try {
 		await fs.promises.access(fileToCompress);
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			console.error(`File does not exist! 🕵️ `);
+		}
+	}
+
+	try {
+		const readableStream = fs.createReadStream(fileToCompress);
+		const writableStream = fs.createWriteStream(compressedFile);
+		const gzip = zlib.createGzip();
+
 		pipeline(readableStream, gzip, writableStream, err => {
 			if (err) {
 				console.error('An error occurred:', err);
 				process.exitCode = 1;
 			} else {
 				console.log('File successfully compressed to archive.gz  👏');
+				fs.promises.unlink(fileToCompress);
 			}
 		});
 	} catch (err) {
-		if (err.code === 'ENOENT') {
-			console.error(`File does not exist! 🕵️ `);
-		}
+		console.error('An unexpected error occurred:', err);
 	}
 };
 
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,52 @@
+/*
+ * implement function that decompresses archive.gz back to the fileToCompress.txt
+ * with same content as before compression using zlib and Streams API
+ */
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+
+	const fileToDecompress = path.join(__dirname, 'files', 'archive.gz');
+	const decompressedFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+	try {
+		await fs.promises.access(fileToDecompress);
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			console.error(`File does not exist! 🕵️ `);
+		}
+	}
+
+	try {
+		const readableStream = fs.createReadStream(fileToDecompress);
+		const writableStream = fs.createWriteStream(decompressedFile);
+		const gunzip = zlib.createGunzip();
+
+		readableStream.on('error', err => {
+			console.error('Error in readable stream:', err);
+		});
+
+		pipeline(readableStream, gunzip, writableStream, err => {
+			if (err) {
+				console.error('An error occurred during decompression:', err);
+				process.exitCode = 1;
+			} else {
+				console.log(
+					'The file was successfully decompressed back to fileToCompress.txt  👏'
+				);
+				fs.promises.unlink(fileToDecompress);
+			}
+		});
+	} catch (err) {
+		console.error('An unexpected error occurred:', err);
+	}
 };
 
 await decompress();
