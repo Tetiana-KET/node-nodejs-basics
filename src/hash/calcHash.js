54f0f8026486ea7ae49b4d4ce76212da795e0cc8
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 14:46:54 +0300

    feat: implement function that calculates SHA256 hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,40 @@
+/*
+calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt 
+and logs it into console as hex using Streams API*/
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+
+	const fileToCalculateHashFor = path.join(
+		__dirname,
+		'files',
+		'fileToCalculateHashFor.txt'
+	);
+
+	const hash = createHash('sha256');
+	const readStream = fs.createReadStream(fileToCalculateHashFor);
+	try {
+		await fs.promises.access(fileToCalculateHashFor);
+
+		readStream.on('data', chunk => {
+			hash.update(chunk);
+		});
+		readStream.on('end', () => {
+			console.log(hash.digest('hex'));
+		});
+		readStream.on('error', err => {
+			console.error('Error reading file:', err);
+		});
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			throw new Error(`FS operation failed.  🕵️  File does not exist!`);
+		}
+	}
 };
 
 await calculateHash();
