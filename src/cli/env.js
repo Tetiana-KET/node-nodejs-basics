319a2167d207c36e836a976a41e3e45a8531c80c
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 12:50:08 +0300

    feat: implement function that parses environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+/*implement function that parses environment variables with prefix RSS_ and prints them to the console 
+in the format RSS_name1=value1; RSS_name2=value2*/
+
 const parseEnv = () => {
-    // Write your code here 
+	const envVariables = process.env;
+	const RSS_variables = Object.entries(envVariables)
+		.filter(([key]) => {
+			return key.startsWith('RSS_');
+		})
+		.map(([key, value]) => {
+			return `${key}=${value}`;
+		})
+		.join('; ');
+	console.log(RSS_variables);
 };
 
 parseEnv();
