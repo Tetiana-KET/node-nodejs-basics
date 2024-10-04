fa76fdf5a8a5ece83756c2fc6085cf34c3f59bb1
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 13:22:21 +0300

    feat: implement function that parses command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+/*implement function that parses command line arguments 
+(given in format --propName value --prop2Name value2, you don't need to validate it)
+ and prints them to the console in the format propName is value, prop2Name is value2*/
 const parseArgs = () => {
-    // Write your code here 
+	const CL_arguments = process.argv.slice(2);
+	const result = [];
+	for (let i = 0; i < CL_arguments.length; i += 2) {
+		const key = CL_arguments[i].replace('--', '');
+		const value = CL_arguments[i + 1];
+		result.push(`${key} is ${value}`);
+	}
+	console.log(result.join(', '));
 };
 
 parseArgs();
