4c149e0245197eb354a7af104b95fa2079acc7c8
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 17:41:30 +0300

    feat: implement function that reads data from process.stdin

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+/*implement function that reads data from process.stdin, reverses text using Transform Stream 
+and then writes it into process.stdout*/
+
+import { Transform } from 'stream';
+
+const reverseStream = new Transform({
+	transform(chunk, _encoding, callback) {
+		const reversedChunk = chunk.toString().split('').reverse().join('');
+		callback(null, reversedChunk);
+	},
+});
+
 const transform = async () => {
-    // Write your code here 
+	process.stdout.write('Hello!, Enter your data:\n');
+	process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
