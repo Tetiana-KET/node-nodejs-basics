e9e88358c6067621d480d431bd66dff0d477ea1f
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-04 15:33:47 +0300

    feat: implement function that reads file fileToRead.txt content using Readable Stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+// /*read.js - implement function that reads file fileToRead.txt content using Readable Stream
+// and prints it's content into process.stdout*/
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+	const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+	const readableStream = fs.createReadStream(fileToRead);
+
+	readableStream.on('data', chunk => process.stdout.write(chunk));
+
+	readableStream.on('error', error => console.log('Error', error.message));
+
+	readableStream.on('end', () =>
+		console.log('\nFile was read successfully! 👏')
+	);
 };
 
 await read();
